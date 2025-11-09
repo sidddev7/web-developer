ddd30f84c3114b17b52ce17e527b916002a8352e
sidddev7 <ggohil2012@gmail.com>
2025-11-09 09:44:21 +0530

    Add personal portfolio data and enhance home page structure

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,20 @@
 /*!
- * Item: Kitzu
- * Description: Personal Portfolio Template
- * Author/Developer: Exill
- * Author/Developer URL: https://themeforest.net/user/exill
- * Version: v2.0.0
- * License: Themeforest Standard Licenses: https://themeforest.net/licenses
+ * Custom JavaScript for Siddhrajsinh Gohil Portfolio
+ * Modern Portfolio Website
  */
-!function(n){"use strict";n((function(){})),n(window).on("load",(function(){}))}(jQuery);
+
+(function($) {
+  'use strict';
+
+  // Initialize when DOM is ready
+  $(document).ready(function() {
+    // Additional custom functionality can be added here
+    console.log('Portfolio website loaded successfully!');
+  });
+
+  // Initialize when window is loaded
+  $(window).on('load', function() {
+    // Any post-load functionality
+  });
+
+})(jQuery);
